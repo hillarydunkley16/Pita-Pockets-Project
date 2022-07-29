ba8770abf2210e3f7ce56fee52dc5172d0e4afc9
hillarydunkley16 <hvgdunkley@gmail.com>
2022-07-29 01:01:14 -0400

    photo carousel

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ for (i=0; i<accordion.length; i++) {
   })
 }
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides(){
+  let i;
+  let slides = document.getElementsByClassName("slide-img");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i<slides.length; i ++){
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if(slideIndex > slides.length){slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
+
+}
+
